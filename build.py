bd799634f135ca5e1df00ae778b2836a2c48e0de
HsOjo <1134031392@qq.com>
2019-03-06 20:27:18 +0800

    release 1.0.0(first)

--- build.py
+++ build.py
@@ -1,5 +1,8 @@
 import os
 import shutil
+from zipfile import ZipFile
+
+from res.const import CONST
 
 # reset dist directory.
 shutil.rmtree('./build')
@@ -16,3 +19,12 @@ dict_pos = info.find('<dict>') + 7
 info = info[:dict_pos] + '\t<key>LSUIElement</key>\n\t<string>1</string>\n' + info[dict_pos:]
 with open(INFO_FILE, 'w', encoding='utf8') as io:
     io.write(info)
+
+zf = ZipFile('./dist/SleeperX-%s.zip' % CONST['version'], 'w')
+src_dir = './dist/SleeperX.app'
+for d, ds, fs in os.walk(src_dir):
+    for f in fs:
+        path = os.path.join(d, f)
+        z_path = path[7:].strip(os.path.sep)
+        zf.write(path, z_path)
+zf.close()
